474778a75e250acd28a3ebe7c3ed9b0294f85d2e
Yifan Ai <ayfallen@hotmail.com>
2020-10-29 18:35:53 +1100

    Implement rule-based chat bot API response

--- chat/chat/v1/api/chat.py
+++ chat/chat/v1/api/chat.py
@@ -2,14 +2,23 @@
 from __future__ import absolute_import, print_function
 
 from flask import request, g
+from rivescript import RiveScript
 
 from . import Resource
-from .. import schemas
+
+bot = RiveScript()
+bot.load_directory("./v1/api/brain/")
+bot.sort_replies()
 
 
 class Chat(Resource):
 
-    def get(self):
-        print(g.args)
-
-        return {}, 200, None
+    @staticmethod
+    def get():
+        message = g.args.get("message")
+        print(f'You> {message}')
+        reply = bot.reply("localuser", message)
+        if reply == "[ERR: No Reply Matched]":
+            reply = "Sorry, I can't understand. Please message me 'hi bot', 'how are you', 'where are you from', 'hi', 'my name is ...', 'i am happy', 'i am excited', 'i am thrilled'."
+        print(f'Bot> {reply}')
+        return {'reply': reply}, 200, None
